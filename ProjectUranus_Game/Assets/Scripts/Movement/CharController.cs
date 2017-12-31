c395d9d9c458a49ef3236865cfa277b0c88b6952
Marco Hitsch <marco.hitsch@gmail.com>
2018-01-01 00:13:59 +0100

    Cleanup + Base Movement

---
+++ ProjectUranus_Game/Assets/Scripts/Movement/CharController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 4f;
+
+    [SerializeField]
+    private float jumpSpeed = 4f;
+    
+    private Vector3 forward, right;
+    private Rigidbody rigidBody;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    forward = Camera.main.transform.forward;
+	    forward.y = 0;
+	    forward = Vector3.Normalize(forward);
+	    right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+	    rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.anyKey)
+	        Move();
+	}
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+        if (Input.GetButtonDown("Jump"))
+            rigidBody.velocity = Vector3.up * jumpSpeed;
+    }
+}
